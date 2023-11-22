8477562ef58b1b1e0382270afb345789d62e10df
Ezra Piyachanokwong <ezrapiya.th@gmail.com>
2023-11-22 13:22:09 +0700

    [Completed]

---
+++ BandNameGenerator.py
@@ -0,0 +1,3 @@
+pet_name_input = input("What's your pet's name? : ")
+city_input = input("What's name of city you grew up in? : ")
+print(f'''Your band name could be {city_input} {pet_name_input}.''')
